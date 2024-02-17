3d9a5f69f32c92a6fbe74a826b412f765edd490a
KK-Repos <iamkishorekumar1@gmail.com>
2024-02-17 19:21:31 +0530

    Added automation test cases for get_word_meaning endpoint

---
+++ test/assertion.py
@@ -0,0 +1,38 @@
+
+def assert_status_code(response, expected_status_code):
+    assert response.status_code == expected_status_code, f"Unexpected status code: {response.status_code}"
+
+def assert_word_meaning(response_data):
+    assert isinstance(response_data, list), "Expected a list in the response"
+    assert len(response_data) == 1, "Expected exactly one item in the list"
+
+    word_info = response_data[0]
+    assert "meanings" in word_info, "Expected 'meanings' key in response"
+    meanings = word_info["meanings"]
+
+    assert isinstance(meanings, list), "Expected 'meanings' to be a list"
+    assert len(meanings) == 1, "Expected exactly one meaning for the word 'car'"
+
+    meaning_info = meanings[0]
+    assert "part_of_speech" in meaning_info, "Expected 'part_of_speech' key in meaning"
+    assert "definitions" in meaning_info, "Expected 'definitions' key in meaning"
+
+    assert meaning_info["part_of_speech"] == "noun", "Expected part of speech to be 'noun'"
+    assert len(meaning_info["definitions"]) == 12, "Expected 13 definitions for the word 'car'"
+
+    source = "https://en.wiktionary.org/wiki/car"
+    assert source in response_data[0].get("source", []), f"Expected source '{source}' not found in the response"
+    
+
+def assert_word_info(word_info, expected_meanings):
+    assert "meanings" in word_info, "Expected 'meanings' key in response"
+    meanings = word_info["meanings"]
+    assert isinstance(meanings, list), "Expected 'meanings' to be a list"
+    assert len(meanings) == expected_meanings, f"Expected exactly {expected_meanings} meaning(s)"
+
+    for meaning_info in meanings:
+        assert "part_of_speech" in meaning_info, "Expected 'part_of_speech' key in meaning"
+        assert "definitions" in meaning_info, "Expected 'definitions' key in meaning"
+
+        part_of_speech = meaning_info["part_of_speech"]
+        assert part_of_speech in ["noun", "verb"], "Expected part of speech to be 'noun' or 'verb'"
---
+++ test/test_instill_qa.py
@@ -0,0 +1,104 @@
+import requests
+import pytest
+
+from test.assertion import assert_status_code, assert_word_info, assert_word_meaning
+
+from app.config import BASE_URL,HOST_PORT,API_V1_STR, PASSWORD
+
+
+
+API_BASE_URL = f"{BASE_URL}:{HOST_PORT}{API_V1_STR}/get-word-meaning"
+
+
+def test_wrong_password_api_Call():
+    payload = {
+        "words": ["car"],
+        "password": "wrong_password"
+    }
+
+    response = requests.post(API_BASE_URL, json=payload)
+
+    assert_status_code(response,403)
+    
+    response_data = response.json()
+    
+
+    assert "detail" in response_data, "Expected 'detail' key in response"
+    assert response_data["detail"] == "Password is incorrect", "Incorrect password error message"
+
+def test_wrong_payload_api():
+    payload = {
+        "words": [0],
+        "password": PASSWORD
+    }
+
+    response = requests.post(API_BASE_URL, json=payload)
+
+    assert response.status_code == 422, f"Unexpected status code: {response.status_code}"
+    
+    response_data = response.json()
+    error_message = response_data['detail'][0]['msg']
+    expected_message = 'Input should be a valid string'
+    assert error_message == expected_message, f"Expected error message: '{expected_message}', but got: '{error_message}'"
+
+    print("[response_data]",error_message)
+
+
+def test_get_word_meaning():
+    payload = {
+        "words": ["car"],
+        "password": PASSWORD
+    }
+
+    response = requests.post(API_BASE_URL, json=payload)
+
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+
+    response_data = response.json()
+
+    assert_word_meaning(response_data)
+
+
+def test_duplicate_payload():
+    payload = {
+        "words": ["car","car"],
+        "password": PASSWORD
+    }
+
+    response = requests.post(API_BASE_URL, json=payload)
+
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+
+    response_data = response.json()
+
+    assert_word_meaning(response_data)
+
+def test_duplicate_payload():
+
+    payload = {
+        "words": ["car", "rocket"],
+        "password": PASSWORD
+    }
+    try:
+        response = requests.post(API_BASE_URL, json=payload)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        assert False, f"Request failed: {e}"
+
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+
+    response_data = response.json()
+
+    assert isinstance(response_data, list), "Expected a list in the response"
+    assert len(response_data) == 2, "Expected exactly two items in the list"
+
+    for word_info in response_data:
+        txt = word_info.get("source", "")
+        if "car" in txt:
+            assert_word_info(word_info, expected_meanings=1)
+        elif "rocket" in txt:
+            assert_word_info(word_info, expected_meanings=2)
+
+
+if __name__ == "__main__":
+    pytest.main(["-v", "test/test_instill_qa.py"])
